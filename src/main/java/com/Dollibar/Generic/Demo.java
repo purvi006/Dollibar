ef129079f1eafa6c155016fd66a6112178bd2f11
Ajaykumar <Ajaykumar@ajaypc>
2020-01-09 08:57:07 +0530

    modified

--- src/main/java/com/Dollibar/Generic/Demo.java
+++ src/main/java/com/Dollibar/Generic/Demo.java
@@ -14,6 +14,7 @@ public class Demo
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://192.168.40.128/dolibarr-3.3.1/htdocs/");
 		Thread.sleep(8000);
+		System.out.println("hello");
 		driver.close();
 	}
 }
